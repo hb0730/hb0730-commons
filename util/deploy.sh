51a21f6db9c2c47ab4524a4cbb1f3d3c2f3070d1
黄兵 <1278032416@qq.com>
2020-08-11 11:30:38 +0800

    update

--- util/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo $TRAVIS_TAG
-
-if [ -z $TRAVIS_TAG ]; then
-  echo "tag is null"
-else
-  echo "Publishing Maven snapshot..."
-  mvn clean deploy --settings=util/settings.xml -DskipTests=true -B -U
-fi
