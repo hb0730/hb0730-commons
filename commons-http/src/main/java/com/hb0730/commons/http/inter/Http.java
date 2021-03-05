bffbc2f70d71037aaafb17b1153eacbb764407ac
黄兵 <1278032416@qq.com>
2021-03-05 11:16:57 +0800

    fixed 删除无用的变量

--- commons-http/src/main/java/com/hb0730/commons/http/inter/Http.java
+++ commons-http/src/main/java/com/hb0730/commons/http/inter/Http.java
@@ -1,13 +1,10 @@
 package com.hb0730.commons.http.inter;
 
-import com.hb0730.commons.http.HttpHeader;
-
 /**
+ * Http interface
+ *
  * @author bing_huang
  * @since 1.0.1
  */
 public interface Http {
-    String HTTP = "http";
-    String HTTPS = "https";
-    HttpHeader DEFAULT_HEADER = HttpHeader.builder();
 }
